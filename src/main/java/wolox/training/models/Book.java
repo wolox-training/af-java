1ee52b948b2c2d7b33868f01a79bb0869d5cd20c
AleFalcon <alex.falcon@wolox.com.ar>
2020-03-06 14:34:48 -0300

    Creation book model

---
+++ src/main/java/wolox/training/models/Book.java
@@ -0,0 +1,133 @@
+package wolox.training.models;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private String genre;
+
+    @Column(nullable = false, unique = true)
+    private String author;
+
+    @Column(nullable = false, unique = true)
+    private String image;
+
+    @Column(nullable = false, unique = true)
+    private String title;
+
+    @Column(nullable = false, unique = true)
+    private String subtitle;
+
+    @Column(nullable = false, unique = true)
+    private String publisher;
+
+    @Column(nullable = false, unique = true)
+    private String year;
+
+    @Column(nullable = false, unique = true)
+    private Integer page;
+
+    @Column(nullable = false, unique = true)
+    private String isbn;
+
+    private long getId() {
+        return id;
+    }
+
+    private String getGenre() {
+        return genre;
+    }
+
+    private String getAuthor() {
+        return author;
+    }
+
+    private String getImage() {
+        return image;
+    }
+
+    private String getTitle() {
+        return title;
+    }
+
+    private String getSubtitle() {
+        return subtitle;
+    }
+
+    private String getPublisher() {
+        return publisher;
+    }
+
+    private String getYear() {
+        return year;
+    }
+
+    private Integer getPage() {
+        return page;
+    }
+
+    private String getIsbn() {
+        return isbn;
+    }
+
+    private void setId(long id) {
+        this.id = id;
+    }
+
+    private void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    private void setAuthor(String author) {
+        this.author = author;
+    }
+
+    private void setImage(String image) {
+        this.image = image;
+    }
+
+    private void setTitle(String title) {
+        this.title = title;
+    }
+
+    private void setSubtitle(String subtitle) {
+        this.subtitle = subtitle;
+    }
+
+    private void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    private void setYear(String year) {
+        this.year = year;
+    }
+
+    private void setPage(Integer page) {
+        this.page = page;
+    }
+
+    private void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Book(String genre, String author, String image, String title, String subtitle, String publisher, String year, Integer page, String isbn) {
+        this.setGenre(genre);
+        this.setAuthor(author);
+        this.setImage(image);
+        this.setTitle(title);
+        this.setSubtitle(subtitle);
+        this.setPublisher(publisher);
+        this.setYear(year);
+        this.setPage(page);
+        this.setIsbn(isbn);
+    }
+}
