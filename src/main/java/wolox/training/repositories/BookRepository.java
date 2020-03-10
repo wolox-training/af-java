a6a2dd2adfec9fe938af4315842c8835070dac42
AleFalcon <alex.falcon@wolox.com.ar>
2020-03-10 15:09:22 -0300

    Modification of find book

--- src/main/java/wolox/training/repositories/BookRepository.java
+++ src/main/java/wolox/training/repositories/BookRepository.java
@@ -1,9 +1,8 @@
 package wolox.training.repositories;
 
-import java.util.List;
 import org.springframework.data.repository.Repository;
 import wolox.training.models.Book;
 
 public interface BookRepository extends Repository<Book, Long> {
-    List<Book> findByAuthor(String author);
+    Book findOneByAuthor(String author);
 }
