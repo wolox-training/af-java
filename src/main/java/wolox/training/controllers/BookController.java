836333d6eb9cfa191fd7e8d3a03b8114ec4cf0e1
AleFalcon <alex.falcon@wolox.com.ar>
2020-03-12 14:20:36 -0300

    Modification returns of all methods

--- src/main/java/wolox/training/controllers/BookController.java
+++ src/main/java/wolox/training/controllers/BookController.java
@@ -26,62 +26,31 @@ public class BookController {
     }
 
     @PostMapping("/create")
-    public String create(@RequestBody Book book, Model model) {
+    public Book create(@RequestBody Book book, Model model) {
         Book new_book = new Book(book.getGenre(), book.getAuthor(), book.getImage(), book.getTitle(), book.getSubtitle(), book.getPublisher(), book.getYear(), book.getPage(), book.getIsbn());
-        bookRepository.save(book);
-        model.addAttribute("genre", new_book.getGenre());
-        model.addAttribute("author", new_book.getAuthor());
-        model.addAttribute("image", new_book.getImage());
-        model.addAttribute("title", new_book.getTitle());
-        model.addAttribute("subtitle", new_book.getSubtitle());
-        model.addAttribute("publisher", new_book.getPublisher());
-        model.addAttribute("year", new_book.getYear());
-        model.addAttribute("page", new_book.getPage().toString());
-        model.addAttribute("isbn", new_book.getIsbn());
-        return "created_book";
+        bookRepository.save(new_book);
+        return new_book;
     }
 
     @PutMapping("/update")
-    public String update(@RequestBody Book book, Model model) {
+    public Book update(@RequestBody Book book, Model model) {
         Book found_book = bookRepository.findByIsbn(book.getIsbn());
         found_book.update(book.getGenre(), book.getAuthor(), book.getImage(), book.getTitle(), book.getSubtitle(), book.getPublisher(), book.getYear(), book.getPage());
-        model.addAttribute("genre", book.getGenre());
-        model.addAttribute("author", book.getAuthor());
-        model.addAttribute("image", book.getImage());
-        model.addAttribute("title", book.getTitle());
-        model.addAttribute("subtitle", book.getSubtitle());
-        model.addAttribute("publisher", book.getPublisher());
-        model.addAttribute("year", book.getYear());
-        model.addAttribute("page", book.getPage().toString());
-        model.addAttribute("isbn", book.getIsbn());
         bookRepository.save(found_book);
 
-        return "modified_book";
+        return found_book;
     }
 
     @DeleteMapping("/delete")
-    public String delete(@RequestParam(name="isbn", required=true) String isbn, Model model) {
+    public Book delete(@RequestParam(name="isbn", required=true) String isbn, Model model) {
         Book book = bookRepository.findByIsbn(isbn);
         bookRepository.delete(book);
-        model.addAttribute("isbn", book.getIsbn());
-        model.addAttribute("title", book.getTitle());
-        model.addAttribute("author", book.getAuthor());
-        model.addAttribute("publisher", book.getPublisher());
-        return "deleted_book";
+        return book;
     }
 
     @GetMapping("/read")
-    public String read(@RequestParam(name="isbn", required=true) String isbn, Model model) {
+    public Book read(@RequestParam(name="isbn", required=true) String isbn, Model model) {
         Book book = bookRepository.findByIsbn(isbn);
-        model.addAttribute("genre", book.getGenre());
-        model.addAttribute("author", book.getAuthor());
-        model.addAttribute("image", book.getImage());
-        model.addAttribute("title", book.getTitle());
-        model.addAttribute("subtitle", book.getSubtitle());
-        model.addAttribute("publisher", book.getPublisher());
-        model.addAttribute("year", book.getYear());
-        model.addAttribute("page", book.getPage().toString());
-        model.addAttribute("isbn", book.getIsbn());
-        return "book_info";
+        return book;
     }
 }
