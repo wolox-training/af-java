b295b3771ad8ea51e6171e5aca495c0a258309f6
AleFalcon <alex.falcon@wolox.com.ar>
2020-03-13 11:41:25 -0300

    Modification by comment

--- src/main/java/wolox/training/repositories/UserRepository.java
+++ src/main/java/wolox/training/repositories/UserRepository.java
@@ -1,8 +1,9 @@
 package wolox.training.repositories;
 
+import java.util.Optional;
 import org.springframework.data.repository.Repository;
 import wolox.training.models.User;
 
 public interface UserRepository extends Repository<User, Long> {
-    User findOneByName(String name);
+    Optional<User> findOneByName(String name);
 }
