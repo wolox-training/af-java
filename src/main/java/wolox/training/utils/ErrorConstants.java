5f4c3d09df81f761d65b80e93de5d95d0ed39275
AleFalcon <alex.falcon@wolox.com.ar>
2020-03-19 13:51:28 -0300

    Corrections by good practise and comments

---
+++ src/main/java/wolox/training/utils/ErrorConstants.java
@@ -0,0 +1,32 @@
+package wolox.training.utils;
+
+public class ErrorConstants {
+
+  //PRECONDITIONS BOOK
+  public static String NOT_NULL_GENRE = "The genre field cannot be null.";
+  public static String NOT_EMPTY_GENRE = "The genre field cannot be empty.";
+  public static String NOT_NULL_AUTHOR = "The author field cannot be null.";
+  public static String NOT_EMPTY_AUTHOR= "The author field cannot be empty.";
+  public static String NOT_NULL_IMAGE = "The image field cannot be null.";
+  public static String NOT_EMPTY_IMAGE = "The image field cannot be empty.";
+  public static String NOT_NULL_TITLE = "The title field cannot be null.";
+  public static String NOT_EMPTY_TITLE = "The title field cannot be empty.";
+  public static String NOT_NULL_SUBTITLE = "The subtitle field cannot be null.";
+  public static String NOT_EMPTY_SUBTITLE = "The subtitle field cannot be empty.";
+  public static String NOT_NULL_PUBLISHER = "The publisher field cannot be null.";
+  public static String NOT_EMPTY_PUBLISHER = "The publisher field cannot be empty.";
+  public static String NOT_NULL_YEAR = "The year field cannot be null.";
+  public static String NOT_NUMERIC = "error message.";
+  public static String NOT_NULL_PAGE = "The author field cannot be null.";
+  public static String NOT_GRADER_THAN_ZERO = "The pages cannot smaller that zero.";
+  public static String NOT_NULL_ISBN = "The isbn field cannot be null.";
+
+  //PRECONDITIONS USER
+  public static String NOT_NULL_NAME = "The name field cannot be null.";
+  public static String NOT_EMPTY_NAME = "The name field cannot be empty.";
+  public static String NOT_NULL_USERNAME = "The username field cannot be null.";
+  public static String NOT_EMPTY_USERNAME = "The username field cannot be empty.";
+  public static String NOT_NULL_BIRTHDAY = "The birthday field cannot be null.";
+  public static String NOT_LATER_CURRENT_DAY = "The birthday cannot be later than the current date.";
+
+}
